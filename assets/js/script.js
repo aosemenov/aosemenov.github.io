d3ce6d617e4778324ccfab7da47ea302097d9707
ar.semenov <ar.semenov@qsoft.ru>
2023-07-23 15:05:22 +0300

    0001 / init project

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/**
+ * Vendors
+ */
+ // import $ from 'jquery';
+ // window.$ = $;
+
+ // Пример подключения плагина
+ // import pluginName from 'plugin-name';
+
+ /**
+  * Modules
+  */
+ // Пример подключения модуля
+ //import module from './modules/module';
+
+
+ const app = {
+    ready() {
+        // Пример вызова импортированных функций
+        // pluginName();
+    },
+
+    load() {
+    },
+
+    resize() {
+
+    },
+
+    scroll() {
+
+    },
+};
+
+
+// $(() => {
+//     app.ready();
+//
+//     $(window)
+//         .on('load', app.load)
+//         .on('resize', app.resize)
+//         .on('scroll', app.scroll);
+// });
